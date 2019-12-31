a0e803d9d69960d7d03fb4d33ef9036c89cdac6a
Leo Wang <bluebreeze0812@outlook.com>
2019-12-31 12:51:41 +0800

    add color buttons

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val views: List<View> = listOf(box_one_text, box_two_text, box_three_text,
-            box_four_text, box_five_text, constraint_layout)
+            box_four_text, box_five_text, green_button, yellow_button, red_button,
+            constraint_layout)
 
         for (view: View in views) {
             view.setOnClickListener {setListener(it)}
@@ -33,6 +34,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
